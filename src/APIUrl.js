218e39b5a5ccdefafe76a4c43644d0b2561de0b4
Nathan Evans <nathan@de1ux.com>
2022-08-17 15:58:03 -0600

    Update to find backend deployed and local backend

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "https://de1ux-blog-backend.herokuapp.com";
+let APIUrl = "http://localhost:3001";
+
+// handle if we're running on heroku 
+if (window.location.host.indexOf('.herokuapp.com') !== -1) {
+    APIUrl = "https://de1ux-blog-backend.herokuapp.com";
+}
 
 export default APIUrl;
